ac71c5b9ba8f444ea6a00104663fd12294a22fa0
thiagoN13 <thiago.nogueira187@gmail.com>
2020-06-26 13:31:42 -0300

    Initial commit

---
+++ public/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './routes/app-routing.module';
+import { AppComponent } from './modules/app/app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
