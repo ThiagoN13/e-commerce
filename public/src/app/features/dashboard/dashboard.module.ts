da78a0c464dd6f53fd560b679ab7e62efecb944f
thiagoN13 <thiago.nogueira187@gmail.com>
2020-07-06 12:43:38 -0300

    Corrigido carrinho

--- public/src/app/features/dashboard/dashboard.module.ts
+++ public/src/app/features/dashboard/dashboard.module.ts
@@ -5,6 +5,10 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { AsideFiltroComponent } from '../../core/components/aside-filtro/aside-filtro.component';
 import { CardItemComponent } from '../../core/components/card-item/card-item.component';
 import { FavoritarProdutoComponent } from '../../core/components/favoritar-produto/favoritar-produto.component';
+import { DetalhesProdutosComponent } from './pages/detalhes-produto/detalhes-produto.component';
+import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
+import { CarrinhoComponent } from './pages/carrinho/carrinho.component';
+import { OfertasComponent } from './pages/ofertas/ofertas.component';
 
 import { SharedModule } from 'src/app/shared/shared.module';
 
@@ -12,9 +16,13 @@ import { SharedModule } from 'src/app/shared/shared.module';
 @NgModule({
   imports: [CommonModule, DashboardRoutingModule, SharedModule],
   declarations: [
-    CardItemComponent,
+    DashboardPageComponent,
+    DetalhesProdutosComponent,
+    OfertasComponent,
+    FavoritarProdutoComponent,
     AsideFiltroComponent,
-    FavoritarProdutoComponent
+    CardItemComponent,
+    CarrinhoComponent
   ],
 })
 
